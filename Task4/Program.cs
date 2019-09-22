43d104b7c8074a958e41dd0be50a82a06a34abee
Руслан Гусейнов <guseinov.ruslan9@mail.ru>
2019-09-22 13:17:51 +0400

    Добавьте файлы проекта.

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int f = 1;
+            Console.WriteLine("Введите число");
+            n = int.Parse(Console.ReadLine());
+
+            for (int c = n; c > 1; c--)
+                f = f * c;
+            Console.WriteLine("Факториал  = " + f);
+            Console.ReadLine();
+        }
+    }
+}
